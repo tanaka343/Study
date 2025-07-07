e70c399917f5e3419b6c07ff50f60cd72bb909bf
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-07-08 08:54:50 +0900

    ダウンロード元を相対パスにする

--- connection_flask_pandas.py
+++ connection_flask_pandas.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template,redirect,send_from_directory,request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def top():
-  df=pd.read_excel(r"C:\Users\Guest User\OneDrive\デスクトップ\【入力シート】全国の放課後等デイサービス事業所リスト作成 のコピー.xlsx",sheet_name='都島区', names=["houjin_name","jigyousyo_name","email","phone","website"])
+  df=pd.read_excel(r"C:\Users\hyom0\OneDrive\デスクトップ\【入力シート】全国の放課後等デイサービス事業所リスト作成.xlsx",sheet_name='都島区', names=["houjin_name","jigyousyo_name","email","phone","website"])
   facility_list=df.to_dict()
   num=len(facility_list)
   return render_template('index.html',facility_list=facility_list,num=num)
@@ -16,7 +17,8 @@ def top():
 # ★ポイント1
 # ex) set DOWNLOAD_DIR_PATH=C:/tmp/flaskDownloadDir
 # DOWNLOAD_DIR_PATH = os.getenv("DOWNLOAD_DIR_PATH")
-DOWNLOAD_DIR_PATH = r"C:\Users\Guest User\OneDrive\デスクトップ\gitHub\Study\自動化アプリ出力"
+base_dir = os.path.dirname(__file__)
+DOWNLOAD_DIR_PATH = os.path.join(base_dir,'自動化アプリ出力')
 
 @app.route('/to_csv', methods=['GET','POST'])
 def to_csv():
