853602e33fae08d9ccad468285a923112b917339
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-07-08 15:23:17 +0900

    sessionによるデータの受け渡し方法学習

--- connection_flask_pandas.py
+++ connection_flask_pandas.py
@@ -1,21 +1,21 @@
-from flask import Flask,render_template,redirect,send_from_directory,request
+from flask import Flask,render_template,redirect,send_from_directory,request,session
 import pandas as pd
 import os
 
 app = Flask(__name__)
+app.secret_key =os.urandom(24).hex()
 
 @app.route('/')
 def top():
-  df=pd.read_excel(r"C:\Users\hyom0\OneDrive\デスクトップ\【入力シート】全国の放課後等デイサービス事業所リスト作成.xlsx",sheet_name='都島区', names=["houjin_name","jigyousyo_name","email","phone","website"])
+  df=pd.read_excel(r"C:\Users\Guest User\OneDrive\デスクトップ\【入力シート】全国の放課後等デイサービス事業所リスト作成 のコピー.xlsx",sheet_name='都島区', names=["houjin_name","jigyousyo_name","email","phone","website"])
   facility_list=df.to_dict()
+  session['my_dataframe']=df.to_json(orient='split')
   num=len(facility_list)
   return render_template('index.html',facility_list=facility_list,num=num)
   
 
 
-# case 2 : send_from_directory()
-# ★ポイント1
-# ex) set DOWNLOAD_DIR_PATH=C:/tmp/flaskDownloadDir
+
 # DOWNLOAD_DIR_PATH = os.getenv("DOWNLOAD_DIR_PATH")
 base_dir = os.path.dirname(__file__)
 DOWNLOAD_DIR_PATH = os.path.join(base_dir,'自動化アプリ出力')
@@ -24,6 +24,9 @@ DOWNLOAD_DIR_PATH = os.path.join(base_dir,'自動化アプリ出力')
 def to_csv():
 
     if request.method=='POST':
+      json_data = session.get('my_dataframe')
+      df=pd.read_json(json_data,orient='split')
+      df.to_csv('自動化アプリ出力/to_csv_out.csv',mode='w')
       downloadFileName ='テスト'
       downloadFile = 'to_csv_out.csv'
       
