0dee5f05c8fb131c66c0fe5e0f62d920027bd647
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-08 00:33:17 +0900

    キーボード操作練習

--- python基礎学習/クリップボード操作/キーボード操作.py
+++ python基礎学習/クリップボード操作/キーボード操作.py
@@ -3,12 +3,29 @@ from pynput import keyboard
 
 keyboard = Controller()
 
-#Aと入力（キーボードを押す離すでワンセット）
-keyboard.press("A")
-keyboard.release("A")
+# #Aと入力（キーボードを押す離すでワンセット）
+# keyboard.press("A")
+# keyboard.release("A")
 
-#shift押しながらaでA
-with keyboard.pressed(Key.shift):
-    keyboard.press("a")
-    keyboard.release("a")
+# #shift押しながらaでA
+# with keyboard.pressed(Key.shift):
+#     keyboard.press("a")
+#     keyboard.release("a")
 
+def on_press(key):
+    try:
+        # キーが文字の場合、その文字を入力
+        keyboard.press(key.char)
+        keyboard.release(key.char)
+    except AttributeError:
+        # 特殊キーの場合はそのまま入力
+        keyboard.press(key)
+        keyboard.release(key)   
+    
+def on_release(key):
+    # Escキーが押されたらリスナーを停止
+    if key == keyboard.Key.esc:
+        return False                
+# キーボードのリスナーを開始
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
