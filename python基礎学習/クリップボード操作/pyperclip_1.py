b9ef4800b042b349691784548f2ed01c3ce55053
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-08 00:32:58 +0900

    クリップボード操作練習

--- python基礎学習/クリップボード操作/pyperclip_1.py
+++ python基礎学習/クリップボード操作/pyperclip_1.py
@@ -1,7 +1,17 @@
 import pyperclip
+import time
+from pynput import keyboard
+# pyperclip.copy('text to be copied')##クリップボードに書き込み
+# print(pyperclip.paste())##クリップボード情報取得
 
-pyperclip.copy('text to be copied')##クリップボードに書き込み
-print(pyperclip.paste())##クリップボード情報取得
-
+clip_list=[]
 pyperclip.copy('')
-print(pyperclip.waitForNewPaste())
+A=pyperclip.paste()
+while True:
+    time.sleep(0.5)
+    a=pyperclip.paste()
+    if a != A:
+        
+        clip_list.append(a)
+        A = a
+        print(clip_list)
